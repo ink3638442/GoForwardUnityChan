b13507008cd065e4cac7ae30aa6b75ce1f68d149
AKIHIRO NAGASHIMA <akihiro.nagashima@gmail.com>
2017-10-17 22:01:46 +0900

    効果音バグの修正

--- Assets/CubeController.cs
+++ Assets/CubeController.cs
@@ -11,12 +11,20 @@ public class CubeController : MonoBehaviour
 
     private float groundLevel = -3.0f;
 
+    // コンポーネント
     AudioSource se;
+    // 効果音
+    AudioClip blockSE;
+
+    // 再生済フラグ
+    private bool isPlayed = false;
+
 
     // Use this for initialization
     void Start()
     {
-        this.se = GetComponent<AudioSource>();
+        this.se      = GetComponent<AudioSource>();
+        this.blockSE = this.se.clip;
     }
 
     // Update is called once per frame
@@ -32,9 +40,13 @@ public class CubeController : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D other)
 	{
-        if (other.gameObject.tag == "GroundTag" || other.gameObject.tag == "CubeTag")
+        // OnCollisionEnter動作時、Cubeの上下で２重判定してしまう防止策として再生済フラグを追加 未再生のキューブのみ対象とする
+        if ((isPlayed == false) && (other.gameObject.tag == "GroundTag" || other.gameObject.tag == "CubeTag"))
         {
-            se.Play();
+            this.se.PlayOneShot(this.blockSE, 1);
+
+            // 再生済フラグをオンにする
+            this.isPlayed = true;
         }
     }
 }
